441c95e1e878809f6ce3ea6841b26ce5dac0e754
JJ <jgg3295@gmail.com>
2017-04-07 11:53:28 -0700

    edit syntax

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 import App from './components/App.jsx';
-import Login from './components/login/Login';
+import Login from './components/login/Login.jsx';
 import SearchContainer from './components/SearchContainer.jsx'
 
 import keys from './config/keys.js';
