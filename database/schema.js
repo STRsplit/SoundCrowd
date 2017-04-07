673386c2720642fe87bb1b5851f9d6a614c3ff0f
JJ <jgg3295@gmail.com>
2017-04-06 17:29:09 -0700

    switch to mysql

---
+++ database/schema.js
@@ -0,0 +1,7 @@
+module.exports = {
+	session: 'CREATE TABLE IF NOT EXISTS sessions (\
+			session_id 		int(11) not null auto_increment primary key,\
+			user_id 			varchar(100) not null default \'a\',\
+			playlist_id   varchar(100) not null default \'a\',\
+	  )'
+};
