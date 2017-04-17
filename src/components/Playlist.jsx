c6b0cd4dbcac325ec749b8f4392eea57adf44752
lg_baum <laura.greenbaum@gmail.com>
2017-04-16 17:45:00 -0700

    Open in Spotify button on all playlists

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import CurrentSongBar from './currentSongBar/CurrentSongBar.jsx';
 import Track from './Track.jsx';
+import { Button } from 'elemental';
 
 class Playlist extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tracks: []
+      tracks: [],
+      owner: ''
     };
     // this.getPlaylistTracks(); 
     this.getPlaylistTracks = this.getPlaylistTracks.bind(this);
@@ -19,36 +21,35 @@ class Playlist extends Component {
 
   getPlaylistTracks() {
     axios.get('/api/playlists/' + this.props.playlist)
-
       .then(res => {
-        let tracks = res.data;
-        this.setState({ tracks: tracks });
+        this.setState({ tracks: res.data.tracks, owner: res.data.owner });
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-
   sortTracks() {
-    var sortedTracks = this.state.tracks.sort((a, b) => {
-      a.vote_count - b.vote_count;
-    })
-    this.setState({ tracks: sortedTracks });
+    // var sortedTracks = this.state.tracks.sort((a, b) => {
+    //   a.vote_count - b.vote_count;
+    // })
+    // this.setState({ tracks: sortedTracks });
   }
-
-  // sortTracks() {
-  // }
 
   render() {
     var id = 0;
     var tracks = this.state.tracks.map(track => (
       <Track key={id++} playlist={this.props.playlist} track={track} getPlaylistTracks={this.getPlaylistTracks} />
-      ));
+    ));
     
     return (
       <div>
         <CurrentSongBar />
+        <div>
+          <a href={`http://open.spotify.com/user/${this.state.owner}/playlist/${this.props.playlist}`}>
+            <Button type="primary"><span>Open in Spotify</span></Button>
+          </a>
+        </div>
         <div>Tracks:{tracks}</div>
       </div>
     )
