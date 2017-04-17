cf76df312c98d70ae078fe0a95fee8861b9c952c
Eugene Penaranda <eugenepenaranda007@gmail.com>
2017-04-17 15:05:17 -0700

    Code cleanup for Playlists components.

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, BrowserHistory, Route, Redirect, Match, Link, Switch } from 'react-router-dom';
 import axios from 'axios';
-
-import Container from 'muicss/lib/react/container';
-import Row from 'muicss/lib/react/row';
-import Col from 'muicss/lib/react/col';
-import Panel from 'muicss/lib/react/panel';
-
 import { Spinner } from 'elemental';
-
 import Paper from 'material-ui/Paper';
 
 class Playlists extends Component {
@@ -27,7 +20,6 @@ class Playlists extends Component {
     })
     .then(res => {
       let playlists = res.data.items;
-      console.log('res.data', res.data);
       this.setState({ playlists, loaded: true });
     })
     .catch(err => {
@@ -82,87 +74,5 @@ const style = {
     textDecoration: 'none'
   }
 };
-
-/*
-
-
-
-//
-<div>{userPlaylists}</div>
-<div>{ this.state.loaded ? userPlaylists : <Spinner />  }</div>
-
-//v2
-
-render() {
-    const userPlaylists = this.state.playlists.map(playlist => {
-    return (
-      <Container fluid={true} style={style.container} >
-      <Row key={playlist.id} style={style.playlist} >
-        <Link to={`/app/playlists/${playlist.id}`} onClick={() => this.setPlaylist(playlist.id)}>
-          <Col md="3" style={style.playlists} >                    
-            <Paper style={style.paper} zDepth={5} >
-              <img src={playlist.images[0].url} style={style.image} />
-            </Paper>
-          </Col>
-        </Link>
-        <Col md="9">
-          Playlist Details
-          {playlist.name}
-        </Col>
-      </Row>  
-      </Container>    
-      )
-    });
-
-    return (
-      <div>
-        <hr />
-        <h2>Playlists:</h2>        
-        <div>
-          { this.state.loaded ? userPlaylists : <Spinner size="lg" />  }
-        </div>
-      </div>
-    );
-  }
-
-  //
-
-
-//ORIG
-  render() {
-    const userPlaylists = this.state.playlists.map(playlist => {
-    return (
-      <div key={playlist.id}>
-        <img src=""/>
-        <Link to={`/app/playlists/${playlist.id}`} onClick={() => this.setPlaylist(playlist.id)}>
-          {playlist.name}
-        </Link>
-      </div>
-      )
-    })
-
-    return (
-      <div>
-        <div><Link to="/app/search">Search</Link></div>
-        <div>
-          <h2>Playlists:</h2>
-          <div>{ this.state.loaded ? userPlaylists : <Spinner size="lg" />  }</div>
-        </div>
-      </div>
-    )
-  }
-
-
-
-
-
-
-
-
-
-*/
-
-
-
 
 
