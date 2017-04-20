9f02385766a0b251a8ce8e14ecb0f950076697cf
JJ <jgg3295@gmail.com>
2017-04-20 15:29:14 -0700

    merge app

--- src/components/playlistSuggester/PlaylistSuggester.jsx
+++ src/components/playlistSuggester/PlaylistSuggester.jsx
@@ -6,86 +6,79 @@ import { connect } from 'react-redux';
 import { setFilters } from '../../actions/filtersActions';
 
 class PlaylistSuggester extends Component {
-
-  componentWillMount() {
-    this.mood = '';
-    this.activity = '';
-    this.location = '';
-    this.weather = '';
-
-    this.fetchFilters = this.fetchFilters.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMood: false,
+      showActivity: false,
+      mood: 'Choose One',
+      activity: 'Choose One'
+    }
     this.setMood = this.setMood.bind(this);
     this.setActivity = this.setActivity.bind(this);
+    this.toggleMood = this.toggleMood.bind(this);
+    this.toggleActivity = this.toggleActivity.bind(this);
     this.findPlaylist = this.findPlaylist.bind(this);
-
-    this.fetchFilters();
   }
 
-  fetchFilters() {
-    axios.get('http://ip-api.com/json')
-    .then(result => {
-      this.location = result['data']['city'];
-      return this.location;
-    })
-    .then(location => {
-      axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + keys['weather'])
-      .then(result => {
-        this.weather = result['data']['weather'][0]['main'];
-      });
-    })
-    .catch(err => console.log('PlaylistSuggester.jsx error componentDidMount: ', err));
-  }   
-
-  findPlaylist() {
-    this.props.setFilters({
-      mood: this.mood,
-      activity: this.activity,
-      location: this.location,
-      weather: this.weather
-    });
-    let count;
-    axios.get('/playlist')
-    .then((result) => {
-      count = result.data.length + 1;
-      axios.post('/create', {number: count});
-    });
-  }
 
   setMood(e) {
-    this.mood = e.target.value;
+    this.props.handleMood(e.target.textContent);
+    this.setState({
+      mood: e.target.textContent
+    });
+    this.toggleMood();
   }
 
   setActivity(e) {
-    this.activity = e.target.value;
+    this.props.handleActivity(e.target.textContent);
+    this.setState({
+      activity: e.target.textContent
+    });
+    this.toggleActivity();
+  }
+
+  toggleMood() {
+    this.setState({
+      showMood: !this.state.showMood
+    });
+  }
+
+  toggleActivity() {
+    this.setState({
+      showActivity: !this.state.showActivity
+    });
   }
 
   render() {
     return (
       <div>
         <div>
-          <Link to='/app/new-playlist' onClick={this.findPlaylist} >
-            Get Suggested Playlist
-          </Link>
+          <h2><Link id="recommended-link" onClick={this.findPlaylist} to='/app/new-playlist'>Get Suggested Playlist</Link></h2>
         </div>
-        <div>
-          <h4>Mood</h4>
-          <select id="mood" onChange={this.setMood}>
-            <option value="Choose One">Choose One</option>
-            <option value="Happy">Happy</option>
-            <option value="Calm">Calm</option>
-            <option value="Sad">Sad</option>
-            <option value="Focused">Focused</option>
-            <option value="Excited">Excited</option>
-          </select>
-          <h4>Activity</h4>
-          <select id="activity" onChange={this.setActivity}>
-            <option value="Choose One">Choose One</option>
-            <option value="Exercising">Exercising</option>
-            <option value="Studying">Studying</option>
-            <option value="Partying">Partying</option>
-            <option value="Chilling">Chilling</option>
-            <option value="Driving">Driving</option>
-          </select>
+        <div id="preferences">
+          <div id="mood">
+            <h3 id="mood-label">Mood</h3>
+            <button id="mood-button" onClick={this.toggleMood}>{this.state.mood}</button>
+            {this.state.showMood ? <div id="mood-menu">
+              <div onClick={this.setMood}>Happy</div>
+              <div onClick={this.setMood}>Calm</div>
+              <div onClick={this.setMood}>Sad</div>
+              <div onClick={this.setMood}>Focused</div>
+              <div onClick={this.setMood}>Excited</div>
+            </div> : null}
+          </div>
+          <div id="activity">
+            <h3 id="activity-label">Activity</h3>
+            <button id="activity-button" onClick={this.toggleActivity}>{this.state.activity}</button>
+            {this.state.showActivity ? <div id="activity-menu">
+              <div onClick={this.setActivity}>Exercising</div>
+              <div onClick={this.setActivity}>Studying</div>
+              <div onClick={this.setActivity}>Partying</div>
+              <div onClick={this.setActivity}>Chilling</div>
+              <div onClick={this.setActivity}>Driving</div>
+            </div> : null}
+          </div>
         </div>
       </div>
     );
@@ -107,3 +100,17 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlaylistSuggester);
+  // fetchFilters() {
+  //   axios.get('http://ip-api.com/json')
+  //   .then(result => {
+  //     this.location = result['data']['city'];
+  //     return this.location;
+  //   })
+  //   .then(location => {
+  //     axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + keys['weather'])
+  //     .then(result => {
+  //       this.weather = result['data']['weather'][0]['main'];
+  //     });
+  //   })
+  //   .catch(err => console.log('PlaylistSuggester.jsx error componentDidMount: ', err));
+  // }   
