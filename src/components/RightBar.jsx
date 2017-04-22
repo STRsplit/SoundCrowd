66da800556cf20b80d3b1cb6555a6f9be2386026
Streed12 <steve.t.reed@gmail.com>
2017-04-21 23:00:01 -0700

    basic structure of right bar in place. Routes, db etc tomorrow.

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+// import { } from '../actions/playlistStatActions';
 
-const RightBar = (props) => (
-  <div className="rightBar-container">
-    <div><h2>Playlist Stats:</h2></div>
-    <div>
-      <div><h3>Recently Added</h3></div>
-      <div><h3>Trending Songs</h3></div>
-      <div><h3>Recently Played</h3></div>
-    </div>
+class RightBar extends Component {
 
-  </div>
-)
+  render() {
+    const { recentlyAddedTracks, recentlyPlayedTracks, trendingTracks } = this.props.playlist
+    return (
+      <div className="rightBar-container">
+        <div><h2>Playlist Stats:</h2></div>
+          <div>
+         <h3>Recently Added</h3>
+          <div>{recentlyAddedTracks.map(track => (
+            <div>{track.title}</div>
+            ))}</div>
+          <h3>Trending Songs</h3>
+          <div>{trendingTracks.map(track => (
+            <div>{track.title}</div>
+            ))}</div>
+          <h3>Recently Played</h3>
+          <div>{recentlyPlayedTracks.map(track => (
+            <div>{track.title}</div>
+            ))}</div>
+        </div>
+      </div>
+    )
+  }
+}
 
-export default RightBar;
+const mapStateToProps = (state) => {
+  return {
+    playlist: state.playlist 
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RightBar);
