fe897f873d1c7e0a1e6a8d3d867818b2a9cbcfcc
JJ <jgg3295@gmail.com>
2017-04-26 16:54:47 -0700

    testing database

--- test/specs.js
+++ test/specs.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var Sequelize = require('sequelize');
 var db = require('../database/db.js');
+var Browser = require('zombie');
 
 describe('Database Schema:', function() {
 	it('contains a User table', function(done) {
@@ -13,4 +14,49 @@ describe('Database Schema:', function() {
 			done(err);
 		});
 	});
+
+	it('contains a Playlist table', function(done) {
+		db.Playlist.findAll()
+		.then(data => {
+			expect(data).to.exist;
+			done();
+		})
+		.catch(err => {
+			done(err);
+		});
+	});
+
+	it('contains a Song table', function(done) {
+		db.Song.findAll()
+		.then(data => {
+			expect(data).to.exist;
+			done();
+		})
+		.catch(err => {
+			done(err);
+		});
+	});
+
+	it('contains a Vote table', function(done) {
+		db.Vote.findAll()
+		.then(data => {
+			expect(data).to.exist;
+			done();
+		})
+		.catch(err => {
+			done(err);
+		});
+	});
 });
+
+describe('Spotify Login:', function() {
+	var browser = new Browser();
+	before(function(done) {
+		browser.visit('http://127.0.0.1:3000/login');
+    browser.pressButton('Log into Spotify');
+    browser.pressButton('LOG IN TO SPOTIFY', done);
+	})
+	it('logs in user with correct password', function(done) {
+
+	});
+})
